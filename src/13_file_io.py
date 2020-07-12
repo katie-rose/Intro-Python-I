4f9009fa096c5ee0fb35b7753c99c2a28c3b7584
Katie Rose Felten <katiefelten@gmail.com>
2020-07-12 18:50:47 -0400

    :zap: updated file I/O

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,8 +10,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-with open('src/foo.txt') as text:
-    print(text.read())
+# fp stands for file pointer
+with open('src/foo.txt') as fp:
+    for line in fp:
+        print(line)
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
